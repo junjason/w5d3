125fd9ca5a81dc9019ed8ed633e9e56ea3ef1b92
Jason Jun <junjason@umich.edu>
2023-09-20 13:04:13 -0400

    done with questions and users orm

--- practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions_database.rb
@@ -25,9 +25,9 @@ class Users
         WHERE
           id = ?
       SQL
-      return nil unless play.length > 0
+      return nil unless user.length > 0
   
-      User.new(play.first) # play is stored in an array!
+      User.new(user.first) # play is stored in an array!
     end
   
     def self.find_by_name(fname,lname)
@@ -39,9 +39,9 @@ class Users
         WHERE
           fname = ? AND lname = ?
       SQL
-      return nil unless play.length > 0
+      return nil unless user.length > 0
   
-      User.new(play.first)
+      User.new(user.first)
     end
   
     def initialize(options)
@@ -52,18 +52,18 @@ class Users
   
     def create
       raise "#{self} already in database" if self.id
-      PlayDBConnection.instance.execute(<<-SQL, self.fname, self.lname)
+      QuestionsDatabase.instance.execute(<<-SQL, self.fname, self.lname)
         INSERT INTO
           users (fname, lname)
         VALUES
           (?, ?, ?)
       SQL
-      self.id = PlayDBConnection.instance.last_insert_row_id
+      self.id = QuestionsDatabase.instance.last_insert_row_id
     end
   
     def update
       raise "#{self} not in database" unless self.id
-      PlayDBConnection.instance.execute(<<-SQL, self.fname, self.lname, self.id)
+      QuestionsDatabase.instance.execute(<<-SQL, self.fname, self.lname, self.id)
         UPDATE
           users
         SET
@@ -72,4 +72,100 @@ class Users
           id = ?
       SQL
     end
-  end
+end
+
+
+class Question
+    attr_accessor :id, :title, :body, :author_id
+  
+    def self.all
+      data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+      data.map { |datum| Questions.new(datum) }
+    end
+  
+    def self.find_by_id(id)
+      question = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+          *
+        FROM
+          questions
+        WHERE
+          id = ?
+      SQL
+      return nil unless question.length > 0
+  
+      Question.new(question.first) # play is stored in an array!
+    end
+  
+    def self.find_by_title(title)
+      question = QuestionsDatabase.instance.execute(<<-SQL, title)
+        SELECT
+          *
+        FROM
+          questions
+        WHERE
+          title = ?
+      SQL
+      return nil unless question.length > 0
+  
+      User.new(question.first)
+    end
+  
+    def self.find_by_body(body)
+        question = QuestionsDatabase.instance.execute(<<-SQL, body)
+          SELECT
+            *
+          FROM
+            questions
+          WHERE
+            body = ?
+        SQL
+        return nil unless question.length > 0
+    
+        User.new(question.first)
+    end
+
+    def self.find_by_author(author_id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+          SELECT
+            *
+          FROM
+            questions
+          WHERE
+            author_id = ?
+        SQL
+        return nil unless question.length > 0
+    
+        User.new(question.first)
+    end
+
+    def initialize(options)
+      @id = options['id']
+      @title = options['title']
+      @body = options['body']
+      @author_id = options['author_id']
+    end
+  
+    def create
+      raise "#{self} already in database" if self.id
+      QuestionsDatabase.instance.execute(<<-SQL, self.title, self.body, self.author_id)
+        INSERT INTO
+          questions (title, body, author_id)
+        VALUES
+          (?, ?, ?, ?)
+      SQL
+      self.id = QuestionsDatabase.instance.last_insert_row_id
+    end
+  
+    def update
+      raise "#{self} not in database" unless self.id
+      QuestionsDatabase.instance.execute(<<-SQL, self.title, self.body, self.author_id, self.id)
+        UPDATE
+          questions
+        SET
+          title = ?, body = ?, author_id = ?
+        WHERE
+          id = ?
+      SQL
+    end
+end
